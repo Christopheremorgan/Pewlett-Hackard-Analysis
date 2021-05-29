935f92ec628bd01cdd8c4ddd91928e4dd79136cf
Chris Morgan <courtneychris@me.com>
2021-05-29 18:18:30 -0400

    ad mentorship eligibility query and csv file

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,3 +29,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(emp_no) DESC
+
+
+-- mentorship eligibility file
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (ti.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (ti.to_date = '9999-01-01')
+ORDER BY (emp_no);
